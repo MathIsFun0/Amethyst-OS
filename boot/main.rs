81982cec73c0297b5289855f9e7f2847573e22b2
Daniel D'Silva <dsfamily9@gmail.com>
2022-03-30 11:41:07 -0400

    please work

---
+++ boot/main.rs
@@ -0,0 +1,50 @@
+#![no_std] // Indicates to the Rust compiler that the app does not depend on the standard library but is a 'standalone' application.
+#![no_main] // Indicates that this application does not have a "main" function typically found in a Linux or Windows application (although it does have its own "main" function "efi_main" as declared below)
+#![feature(alloc_error_handler)] // Needed for the alloc error handler function declared below since this feature is unstable.
+
+// Externs for efi and alloc crates (alloc crate is the one that contains definitions of String and Vec etc.)
+#[macro_use] extern crate efi;
+#[macro_use] extern crate alloc;
+
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+
+
+// EFI entrypoint or main function. UEFI firmware will call this function to start the application.
+// The signature and the name of this function must be exactly as below.
+#[no_mangle]
+pub extern "win64" fn efi_main(image_handle: efi::ffi::EFI_HANDLE, sys_table : *const efi::ffi::EFI_SYSTEM_TABLE) -> isize {
+    efi::init_env(image_handle, sys_table); // Call to init_env must be the first thing in efi_main. Without it things like println!() won't work
+    //efi::console::Console::set_fore_color(efi::console::ForeColor.LightMagenta);
+    //efi::console::Console::set_back_color(efi::console::BackColor.Magenta);
+    //^ Can't get these two lines to work. I probably have to initialize console first.
+    println!("
+    ___                   __  __               __ 
+   /   |  ____ ___  ___  / /_/ /_  __  _______/ /_
+  / /| | / __ `__ \\/ _ \\/ __/ __ \\/ / / / ___/ __/
+ / ___ |/ / / / / /  __/ /_/ / / / /_/ (__  ) /_  
+/_/  |_/_/ /_/ /_/\\___/\\__/_/ /_/\\__, /____/\\__/  
+                                /____/            
+Amethyst OS ©2022 Alpha v0.0.1\r\n
+"); //some double backslashes here but besides that it's just text art
+
+    let mut f = File::open("somefile.txt");
+    let mut buffer = [0; 10];
+    f.read(&mut buffer);
+    println!("The bytes: {:?}". buffer);
+
+    0
+}
+
+// A handler to respond to panics in the code. Required by the Rust compiler
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+// A handler to respond to allocation failures. Required by the Rust compiler
+#[alloc_error_handler]
+fn alloc_error(_: core::alloc::Layout) -> ! {
+    loop {}
+}
